6c3d1e6e9a10e84d116e5a6d8c3f515557a2b534
nyeka <nyeka2101@gmail.com>
2023-09-07 12:07:13 +0700

    feat: adding vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './tailwind.css'
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
 
 app.use(router)
+inject()
 
 app.mount('#app')
